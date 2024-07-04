772f15a06883b7794c8b128384822f901953565b
Andrey Ivanov <this.andrey@gmail.com>
2024-07-04 23:13:41 +0300

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "lame",
+    platforms: [
+        .iOS(.v12),
+        .tvOS(.v12),
+        .macOS(.v10_14),
+        .macCatalyst(.v10_14)
+    ],
+    products: [
+        .library(
+            name: "lame",
+            targets: ["lame"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "lame",
+            path: "./lame.xcframework"
+        ),
+    ]
+)
