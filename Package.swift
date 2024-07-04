e2261224a8e115d193e898cd54c2cc741685108f
Andrey Ivanov <this.andrey@gmail.com>
2024-07-05 00:31:24 +0300

    New lame version 1.2.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version:5.4
 import PackageDescription
 
 let package = Package(
@@ -10,15 +10,14 @@ let package = Package(
         .macCatalyst(.v10_14)
     ],
     products: [
-        .library(
-            name: "lame",
-            targets: ["lame"]),
+        .library(name: "lame", targets: ["lame"]),
     ],
+    dependencies: [],
     targets: [
         .binaryTarget(
             name: "lame",
             url: "https://github.com/pro100andrey/lame/releases/download/1.2.1/lame.xcframework.zip",
-            checksum: "8da284cb6d36ebb157a6ed68a0c5fd5842530a2b1b40e7f5dedcb8b6bbdc3a19"
+            checksum: "ed0d0249e1b3476de437090e933c56af1d849d2957e0415b54c7b7173a841228"
         )
     ]
 )
