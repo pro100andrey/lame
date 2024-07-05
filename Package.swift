32fc4c5c3101f56b6f126e72ba9e689c5f57d1fc
Andrey Ivanov <this.andrey@gmail.com>
2024-07-05 14:15:16 +0300

    New lame version 1.2.2

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
             name: "lame",
 
             url: "https://github.com/pro100andrey/lame/releases/download/1.2.2/lame.xcframework.zip",
-            checksum: "622e43b1386f266af4a481aa197d2a5e4572849bbab0119d84608d672063c6a5"
+            checksum: "32cba1c70f4b970c9a4230c50da9bc2a6d75e65212a7f9def9c0b23577924129"
         )
     ]
 )
